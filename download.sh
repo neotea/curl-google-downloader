bd8d14eb9f7559e726a689577a8b0a97b4b59e53
Sören Heinzmann <Soeren@MacBook-Pro.fritz.box>
2017-12-11 15:39:10 +0100

    init

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -n "$1" ]
+  then
+    ggID=$1 && \
+    ggURL='https://drive.google.com/uc?export=download' && \
+    filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')" && \
+    getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)" &&  \
+    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}" 
+fi
